0857ab2172366b5c8c2313338bf02ac51b4ea44f
justinenorie <justinedelacruz97@gmail.com>
2025-06-28 22:39:48 +0800

    fix: bug fix of not checking the accessToken

--- apps/backend/src/middlewares/verifyAdmin.ts
+++ apps/backend/src/middlewares/verifyAdmin.ts
@@ -1,20 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 import { verifyAccessToken } from "../utils/jwtHandler";
 
-export const verifyAdmin = (
+export const verifyAdmin = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {
+): Promise<any> => {
   const authHeader = req.headers.authorization;
-  if (!authHeader) return res.sendStatus(401);
+  if (!authHeader) {
+    console.log("Missing Authorization header");
+    return res.sendStatus(401);
+  }
 
   const token = authHeader.split(" ")[1];
-  const decoded = verifyAccessToken(token) as any;
+  try {
+    const decoded = verifyAccessToken(token) as any;
+    if (decoded.role !== "admin") {
+      console.log("Forbidden: Not admin");
+      return res.sendStatus(403);
+    }
 
-  if (decoded.role !== "admin") return res.sendStatus(403);
-
-  // Attach admin ID to request for later use
-  (req as any).adminId = decoded.id;
-  next();
+    (req as any).adminId = decoded.id;
+    next();
+  } catch (err) {
+    console.log("Invalid token", err);
+    res.sendStatus(403);
+  }
 };
