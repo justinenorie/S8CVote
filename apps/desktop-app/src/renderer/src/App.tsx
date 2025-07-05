5f82125115009be56c893cc23d8d54cf54d88969
justinenorie <justinedelacruz97@gmail.com>
2025-07-05 10:13:11 +0800

    refactor: remove current styles and assets and integrate Tailwind CSS for desktop-app

--- apps/desktop-app/src/renderer/src/App.tsx
+++ apps/desktop-app/src/renderer/src/App.tsx
@@ -1,35 +1,5 @@
-import Versions from './components/Versions'
-import electronLogo from './assets/electron.svg'
+const App = (): React.JSX.Element => {
+  return <div className="bg-red-500">Hey This is Design</div>;
+};
 
-function App(): React.JSX.Element {
-  const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
-
-  return (
-    <>
-      <img alt="logo" className="logo" src={electronLogo} />
-      <div className="creator">Can I edit here</div>
-      <div className="text">
-        Hehe an Electron app with <span className="react">React</span>
-        &nbsp;and <span className="ts">TypeScript</span>
-      </div>
-      <p className="tip">
-        Please try pressing <code>F12</code> to open the devTool
-      </p>
-      <div className="actions">
-        <div className="action">
-          <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">
-            Documentation
-          </a>
-        </div>
-        <div className="action">
-          <a target="_blank" rel="noreferrer" onClick={ipcHandle}>
-            Send IPC
-          </a>
-        </div>
-      </div>
-      <Versions></Versions>
-    </>
-  )
-}
-
-export default App
+export default App;
