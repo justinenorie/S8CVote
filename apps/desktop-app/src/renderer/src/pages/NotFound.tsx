c7240b16444b6b468e68f560b1580f11b72a024f
justinenorie <justinedelacruz97@gmail.com>
2025-09-15 07:31:49 +0800

    feat: Refactor NotFound page to use Typography and Button components for consistency

--- apps/desktop-app/src/renderer/src/pages/NotFound.tsx
+++ apps/desktop-app/src/renderer/src/pages/NotFound.tsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
+import Typography from "@renderer/components/ui/Typography";
+import { Button } from "@renderer/components/ui/Button";
 
 const NotFound = (): React.JSX.Element => {
   return (
-    <div className="flex h-full flex-col items-center justify-center text-center">
-      <h1 className="mb-4 text-6xl font-bold text-red-500">404</h1>
-      <p className="mb-2 text-xl">Oops! Page not found.</p>
-      <p className="mb-6 text-gray-500">
-        The page you are looking for does not exist or has been moved.
-      </p>
-
-      <Link
-        to="/dashboard"
-        className="bg-primary hover:bg-primary/90 rounded-lg px-6 py-3 text-white transition"
+    <div className="text-TEXTdark dark:text-TEXTlight flex h-full flex-col items-center justify-center text-center">
+      <Typography
+        variant="h1"
+        className="text-ERRORlight dark:text-ERRORlight mb-4 text-8xl font-bold"
       >
-        ⬅ Back to Dashboard
-      </Link>
+        404
+      </Typography>
+      <Typography variant="h3" className="mb-2">
+        Oops! Page not found.
+      </Typography>
+      <Typography variant="p" className="mb-6 text-gray-500">
+        The page you are looking for does not exist or has been moved.
+      </Typography>
+
+      <Button variant="default">
+        <Link to="/dashboard" className="rounded-lg text-white transition">
+          ⬅ Back to Dashboard
+        </Link>
+      </Button>
     </div>
   );
 };
