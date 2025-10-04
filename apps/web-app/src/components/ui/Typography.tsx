38c2fedf61abdb9a89a1702bf116b654cdaebf31
justinenorie <justinedelacruz97@gmail.com>
2025-10-04 21:18:25 +0800

    feat: implement Typography component

--- apps/web-app/src/components/ui/Typography.tsx
+++ apps/web-app/src/components/ui/Typography.tsx
@@ -1,5 +1,33 @@
-import React from "react";
+import React, { ReactNode } from "react";
+import { cn } from "@/lib/utils";
 
-export const Typography = () => {
-  return <div>Typography</div>;
+type Variant = "h1" | "h2" | "h3" | "h4" | "p" | "small";
+
+interface TypographyProps {
+  variant?: Variant;
+  children: ReactNode;
+  className?: string;
+}
+
+const Typography: React.FC<TypographyProps> = ({
+  variant = "p",
+  children,
+  className = "",
+}) => {
+  const baseStyles: Record<Variant, string> = {
+    h1: "font-title font-bold text-4xl",
+    h2: "font-title font-semibold text-3xl",
+    h3: "font-title font-medium text-2xl",
+    h4: "font-title font-medium text-xl",
+    p: "font-body text-base leading-7",
+    small: "font-body text-sm leading-none",
+  };
+
+  return React.createElement(
+    variant,
+    { className: cn(`${baseStyles[variant]} ${className}`) },
+    children
+  );
 };
+
+export default Typography;
