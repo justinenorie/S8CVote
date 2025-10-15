8bc65866b685f82494bd9d43d79b34c1399e4b67
justinenorie <justinedelacruz97@gmail.com>
2025-10-15 12:19:23 +0800

    feat: implemented a state management for syncing the candidates data

--- apps/desktop-app/src/renderer/src/stores/useCandidateStore.ts
+++ apps/desktop-app/src/renderer/src/stores/useCandidateStore.ts
@@ -11,6 +11,10 @@ interface CandidateState {
   loading: boolean;
   error: string | null;
 
+  syncing: boolean;
+  syncError: string | null;
+  lastSyncedAt: string | null;
+
   fetchCandidates: () => Promise<Result<Candidates[]>>;
   addCandidate: (
     candidate: Omit<Candidates, "id" | "election">
@@ -20,6 +24,9 @@ interface CandidateState {
     updates: Partial<Candidates>
   ) => Promise<Result<Candidates>>;
   deleteCandidate: (id: string) => Promise<Result<null>>;
+  syncToServerCandidates: () => Promise<Result<null>>;
+  syncFromServerCandidates: () => Promise<Result<null>>;
+  fullSyncCandidates: () => Promise<Result<null>>;
 }
 
 export const useCandidateStore = create<CandidateState>((set, get) => ({
@@ -27,43 +34,55 @@ export const useCandidateStore = create<CandidateState>((set, get) => ({
   loading: false,
   error: null,
 
+  syncing: false,
+  syncError: null,
+  lastSyncedAt: null,
+
   // FETCH
   fetchCandidates: async () => {
     set({ loading: true, error: null });
 
-    const { data, error } = await supabase.from("candidates").select(
-      `
-        id,
-        name,
-        description,
-        profile,
-        election_id,
-        election:election_id (
-          id,
-          election,
-          status
-        )
-      `
-    );
-    if (error) {
-      console.error("Error fetching candidates:", error);
-      set({ error: error.message, loading: false });
-      return { data: null, error: error.message };
+    try {
+      const data = await window.electronAPI.candidatesGet();
+
+      // const { data, error } = await supabase.from("candidates").select(
+      //   `
+      //     id,
+      //     name,
+      //     description,
+      //     profile,
+      //     election_id,
+      //     election:election_id (
+      //       id,
+      //       election,
+      //       status
+      //     )
+      //   `
+      // );
+      // if (error) {
+      //   console.error("Error fetching candidates:", error);
+      //   set({ error: error.message, loading: false });
+      //   return { data: null, error: error.message };
+      // }
+
+      const candidatesData: Candidates[] = data.map((c) => {
+        return {
+          id: c.id,
+          name: c.name,
+          description: c.description,
+          profile: c.profile,
+          election_id: c.election_id,
+          election: Array.isArray(c.election) ? c.election[0] : c.election,
+        };
+      });
+
+      set({ candidates: candidatesData, loading: false });
+      return { data: candidatesData, error: null };
+    } catch (error: unknown) {
+      console.error("Fetch elections error:", error);
+      set({ error: error as string, loading: false });
     }
-
-    const candidatesData: Candidates[] = data.map((c) => {
-      return {
-        id: c.id,
-        name: c.name,
-        description: c.description,
-        profile: c.profile,
-        election_id: c.election_id,
-        election: Array.isArray(c.election) ? c.election[0] : c.election, // ✅ fix typing
-      };
-    });
-
-    set({ candidates: candidatesData, loading: false });
-    return { data: candidatesData, error: null };
+    return { data: null, error: "" };
   },
 
   // ADD
@@ -119,7 +138,59 @@ export const useCandidateStore = create<CandidateState>((set, get) => ({
     await get().fetchCandidates();
     return { data: null, error: null };
   },
+
   // SYNC METHODS HERE FOR CANDIDATES
-  // syncToServerCandidates
-  // syncFromServerCandidates
+  // SYNC TO SERVER
+  syncToServerCandidates: async () => {
+    try {
+      const unsynced = await window.electronAPI.candidatesGetUnsynced();
+      if (!unsynced || unsynced.length === 0)
+        return { data: null, error: null };
+
+      const payload = unsynced.map((c) => ({ ...c, synced_at: 1 }));
+
+      const { error } = await supabase.from("candidates").upsert(payload);
+      if (error) throw error;
+
+      const ids = unsynced.map((c) => c.id);
+      await window.electronAPI.candidatesMarkSynced(ids);
+
+      return { data: null, error: null };
+    } catch (error: unknown) {
+      console.error("Fetch elections error:", error);
+      set({ error: error as string, loading: false });
+    }
+    return { data: null, error: "" };
+  },
+
+  // SYNC FROM SERVER
+  syncFromServerCandidates: async () => {
+    try {
+      const { data, error } = await supabase.from("candidates").select("*");
+      if (error) throw error;
+
+      await window.electronAPI.candidatesBulkUpsert(data);
+      await get().fetchCandidates();
+    } catch (error: unknown) {
+      console.error("Fetch elections error:", error);
+      set({ error: error as string, loading: false });
+    }
+    return { data: null, error: "" };
+  },
+
+  fullSyncCandidates: async () => {
+    set({ syncing: true, syncError: null });
+    try {
+      await get().syncToServerCandidates();
+      await get().syncFromServerCandidates();
+      set({ lastSyncedAt: new Date().toISOString() });
+    } catch (error: unknown) {
+      console.error("Fetch elections error:", error);
+      set({ error: error as string, loading: false });
+    } finally {
+      set({ syncing: false });
+    }
+
+    return { data: null, error: "" };
+  },
 }));
